89535dd92fbb16e3478a6a08eb958f11f75585dd
Volodymyr Zvarun <lolpie2651@gmail.com>
2025-05-20 13:17:15 +0300

    Make map area transparent

--- map/map.gd
+++ map/map.gd
@@ -3,13 +3,14 @@ extends MeshInstance3D
 @onready var tile_manager = $TilesManager
 
 var test_map = [
-	['w', 'w', 'w', 'w', 'w', 'w'],
-	['w', 'f', 'f', 'w', 'f', 'w'],
-	['w', 'f', 'f', 'w', 'f', 'w'],
-	['w', 'w', 'w', 'w', 'w', 'w'],
-	['w', 'f', 'f', 'w', 'f', 'w'],
-	['w', 'w', 'w', 'w', 'w', 'w'],
+	["w", "w", "w", "w", "w", "w"],
+	["w", "f", "f", "w", "f", "w"],
+	["w", "f", "f", "w", "f", "w"],
+	["w", "w", "w", "w", "w", "w"],
+	["w", "f", "f", "w", "f", "w"],
+	["w", "w", "w", "w", "w", "w"],
 ]
+
 
 func _convert_map(map: Array) -> Array:
 	var result = map.duplicate()
@@ -17,9 +18,9 @@ func _convert_map(map: Array) -> Array:
 	for i in map.size():
 		for j in map[i].size():
 			match map[i][j]:
-				'w':
+				"w":
 					result[i][j] = Tile.Type.Wall
-				'f':
+				"f":
 					result[i][j] = Tile.Type.Floor
 				_:
 					push_warning("Unknown tile: on position [%, %]" % i, j)
@@ -27,9 +28,12 @@ func _convert_map(map: Array) -> Array:
 
 	return result
 
+
 func _ready() -> void:
 	var plane_mesh := self.mesh as PlaneMesh
-	plane_mesh.size = Vector2(self.get_aabb().size.x * self.scale.x, self.get_aabb().size.z * self.scale.z)
+	plane_mesh.size = Vector2(
+		self.get_aabb().size.x * self.scale.x, self.get_aabb().size.z * self.scale.z
+	)
 	self.scale = Vector3(1, 1, 1)
 
 	set_map(test_map)
@@ -39,10 +43,12 @@ func set_map(raw_map):
 	var map = _convert_map(raw_map)
 
 	var tile_size = min(self.get_aabb().size.z / map.size(), self.get_aabb().size.x / map[0].size())
-	var start_point = -self.get_aabb().size / 2 + Vector3(tile_size / 2, 0, tile_size / 2)
+	var start_point = (
+		-Vector3(map[0].size() * tile_size / 2, 0, map.size() * tile_size / 2)
+		+ Vector3(tile_size / 2, 0, tile_size / 2)
+	)
 
 	var tile_pos = Vector3(tile_size, self.position.y, tile_size)
-
 
 	for y in range(map.size()):
 		for x in range(map[y].size()):
