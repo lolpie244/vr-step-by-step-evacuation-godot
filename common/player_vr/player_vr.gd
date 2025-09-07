9c2cc810f856b9e53ec9a1621f8ba2f8156b5531
Volodymyr Zvarun <lolpie2651@gmail.com>
2025-09-07 15:34:17 +0300

    Import images on quest

--- common/player_vr/player_vr.gd
+++ common/player_vr/player_vr.gd
@@ -9,6 +9,3 @@ var	xr_interface: OpenXRInterface = XRServer.find_interface("OpenXR")
 
 func _ready() -> void:
 	xr_interface.set_action_set_active("strategic", true)
-
-func _process(_delta):
-	print("Stick valu123e ", LeftHand.get_vector2("move"))
