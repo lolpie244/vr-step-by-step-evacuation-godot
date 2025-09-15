fee556c08ea16e6e9c15eb1ee2bd647a86bdf881
Volodymyr Zvarun <lolpie2651@gmail.com>
2025-09-15 19:57:53 +0300

    Add better fire

--- map/tiles/mixins/flammable.gd
+++ map/tiles/mixins/flammable.gd
@@ -3,6 +3,7 @@ extends TileMixin
 class_name Flammable
 
 @export var material: TileMaterial
+@onready var fire: FireEffect = $Fire
 
 var burning: bool = false
 var burned: bool = false
@@ -11,22 +12,15 @@ var burned: bool = false
 func _ready():
 	hide()
 
-
 func init() -> void:
-	scale(_tile.scale.x)
+	pass
+	#scale(_tile.scale.x)
 
-
-func scale(scale_: float):
-	var process_material: ParticleProcessMaterial = $Particles.process_material
-	process_material.scale_max = scale_
-	process_material.scale_min = scale_
-	process_material.emission_shape_scale = Vector3.ONE * scale_
-	process_material.gravity *= scale_
-
+func cooling_percentage(val):
+	fire.cooling_coef = val
 
 func can_burn():
 	return !burning && !burned
-
 
 func ignite():
 	burning = true
